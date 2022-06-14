cd64026b9c29e7eb4d086929f1e3237c8f037c2b
Kenyansa <kenyansajustine@gmail.com>
2022-06-15 00:32:39 +0300

    completed assingment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
   # your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(avi_attributes)
+    avi_attributes.each do |key, value|
+      self.send("#{key}=", value)
+    end
+  end
+
+  #refactored:
+  # def initialize(avi_attributes = nil)
+  #   if avi_attributes
+  #     avi_attributes.each do |key, value|
+  #       self.class.attr_accessor(key)
+  #       self.send("#{key}=", value)
+  #     end
+  #   end
+  # end
 end
